901e262963499ac848e73b94b69130ebb60e96b4
Yann Rolland <yann.rolland@iadvize.com>
2022-06-11 16:09:26 +0200

    wip

--- app.go
+++ app.go
@@ -278,8 +278,18 @@ func findWinningMoves(state State) []Move {
 
 		legalMoves := findLegalMoves(node.state)
 
-		if len(node.history) > 0 && (node.history[len(node.history)-1] == Move{piece: whiteRook, from: Coord{5, 5}, to: Coord{5, 7}}) {
-			debug("Path", node.history, "state", node.state, "legalMoves", legalMoves, "checkMated", checkMated)
+		if len(node.history) > 0 {
+			lastMove := node.history[len(node.history)-1]
+			if lastMove.piece == whiteKing && node.state.whiteKing != lastMove.to {
+				debug("Path", node.history, "state", node.state, "legalMoves", legalMoves, "checkMated", checkMated)
+				panic("wrong white king")
+			} else if lastMove.piece == blackKing && node.state.blackKing != lastMove.to {
+				debug("Path", node.history, "state", node.state, "legalMoves", legalMoves, "checkMated", checkMated)
+				panic("wrong black king")
+			} else if lastMove.piece == whiteRook && node.state.whiteRook != lastMove.to {
+				debug("Path", node.history, "state", node.state, "legalMoves", legalMoves, "checkMated", checkMated)
+				panic("wrong white rook")
+			}
 		}
 
 		for _, move := range legalMoves {
